3c75d86ed909d15eb24b959307e7d202dd8d3afc
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-05-01 08:49:41 +0300

    fixed svg component for IE

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,5 @@
 import React from 'react';
-// components
-import { ReactComponent as Phone } from '../../assets/svg/phone.svg';
-import { ReactComponent as Email } from '../../assets/svg/envelope.svg';
-import { ReactComponent as Home } from '../../assets/svg/home.svg';
+
 // styles
 import styles from './Footer.module.css';
 
@@ -15,15 +12,12 @@ const Footer = () => (
 
       <ul className={styles.contacts}>
         <li className={styles.phone}>
-          <Phone />
           <p>0981 / 48 77 231</p>
         </li>
         <li className={styles.mail}>
-          <Email />
           <p>Jochsem.mueller@hs-ansbach.de</p>
         </li>
         <li className={styles.home}>
-          <Home />
           <p>FH Ansbach Residenzstr. 8 91522 Ansbach</p>
         </li>
       </ul>
