150a2da16e58e2baa5b435a1c299530a7288c2e3
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-06-05 10:12:17 +0300

    package.json

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
